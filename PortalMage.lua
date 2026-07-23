9771c4e5c849c65c73dbaf12a683f1872767aae8
Wojciech Guść <vampyr7878@gmail.com>
2026-07-23 11:53:01 +0200

    Updated for 1.15.9

--- PortalMage.lua
+++ PortalMage.lua
@@ -558,7 +558,7 @@ function PortalMage.frameOnEvent(self, event, spell)
 			group:ReSkin()
 		end
 		self:UnregisterEvent("PLAYER_LOGIN")
-	elseif event == "LEARNED_SPELL_IN_TAB" and PortalMage:NewSpell(spell) then
+	elseif event == "LEARNED_SPELL_IN_SKILL_LINE" and PortalMage:NewSpell(spell) then
 		self:SetHeight(40)
 		PortalMage.move:SetHeight(40);
 		if portalMageData.Vertical then
@@ -619,7 +619,7 @@ function PortalMage:SetupFrame(frame)
 	frame:SetScale(0.8 * portalMageData.Scale)
 	frame:SetFrameStrata("LOW")
 	frame:RegisterEvent("PLAYER_LOGIN")
-	frame:RegisterEvent("LEARNED_SPELL_IN_TAB")
+	frame:RegisterEvent("LEARNED_SPELL_IN_SKILL_LINE")
 	frame:RegisterEvent("BAG_UPDATE")
 	frame:SetScript("OnEvent", PortalMage.frameOnEvent)
 	frame.teleports = frame:CreateFontString(nil, "OVERLAY", "GameFontWhite")
